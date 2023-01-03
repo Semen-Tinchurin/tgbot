7bba9797a91699e65e31d18b2d57bbf08c83c302
sam <sementinchurin@gmail.com>
2023-01-03 13:26:40 +0300

    code refactor and error handling

---
+++ main.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from config import TOKEN
+import logging
+from music import *
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    level=logging.INFO)
+
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Bot started...')
+
+
+def cancel(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='See ya...')
+    return ConversationHandler.END
+
+
+# Handlers
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+ch = ConversationHandler(entry_points=[CommandHandler('getmp3', get_link)],
+                         states={
+                             EXPECT_LINK: [MessageHandler(Filters.text, link_input_by_user)],
+                         },
+                         fallbacks=[CommandHandler('cancel', cancel)])
+dispatcher.add_handler(ch)
+end_handler = CommandHandler('cancel', cancel)
+dispatcher.add_handler(end_handler)
+
+updater.start_polling()
+updater.idle()
--- music.py
+++ music.py
@@ -1,27 +1,12 @@
-import logging
 from telegram import Update
-from telegram.ext import Updater, CallbackContext, CommandHandler, \
-    MessageHandler, Filters, ConversationHandler, filters
-from config import TOKEN
+from telegram.ext import CallbackContext, ConversationHandler
 import re
+import youtube_dl
+import os
 
 # re.match('https:\/\/www\.youtube\.[a-z]{2,}\/watch\?v=([A-Za-z0-9-_\&]+)', url)
 
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                    level=logging.INFO)
-
 EXPECT_LINK = range(1)
-
-
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Bot started...')
-
-
-def echo(update: Update, context: CallbackContext):
-    message_text = update.message.text
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"reply - {message_text}")
 
 
 def get_link(update: Update, context: CallbackContext):
@@ -32,35 +17,41 @@ def get_link(update: Update, context: CallbackContext):
 def link_input_by_user(update: Update, context: CallbackContext):
     link = update.message.text
     if re.match('https:\/\/www\.youtube\.[a-z]{2,}\/watch\?v=([A-Za-z0-9-_\&]+)', link):
-        update.message.reply_text(f'Your link is VALID')
-        update.message.reply_text(f'Start download')
-        download_music(link=link)
+        update.message.reply_text('Start download')
+        download_music(update, context, link=link)
     else:
         update.message.reply_text("Your link is INVALID or it's not a link!!!!")
     # ends this particular conversation flow
     return ConversationHandler.END
 
 
-def download_music(link):
-    pass
+def download_music(update: Update, context: CallbackContext, link):
+    video_info = youtube_dl.YoutubeDL().extract_info(url=link, download=False)
+    title = video_info['title'].replace(' ', '_')
 
+    #clear the title
+    if "_[Lyric_Video]" in title:
+        title = title.replace("_[Lyric_Video]", '')
+    if "_(Official_Video)" in title:
+        title = title.replace("_(Official_Video)", '')
 
-def cancel(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='See ya...')
-    return ConversationHandler.END
-
-
-# Handlers
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-ch = ConversationHandler(entry_points=[CommandHandler('getmp3', get_link)],
-                         states={
-                             EXPECT_LINK: [MessageHandler(Filters.text, link_input_by_user)],
-                         },
-                         fallbacks=[CommandHandler('cancel', cancel)])
-dispatcher.add_handler(ch)
-end_handler = CommandHandler('cancel', cancel)
-dispatcher.add_handler(end_handler)
-
-updater.start_polling()
-updater.idle()
+    file_path = f"music/{title}.mp3"
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': file_path,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        try:
+            ydl.download([video_info['webpage_url']])
+        except Exception as ex:
+            print(ex.args, ex)
+        finally:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Downloaded, here is your music:')
+            context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(file_path, 'rb'))
+            os.remove(file_path)
